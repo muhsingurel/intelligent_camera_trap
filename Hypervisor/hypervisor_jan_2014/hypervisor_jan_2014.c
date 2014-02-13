b4eeeee316b1b920b18ad6c484dd6c15533504cd
muhsingurel <muhsingurel@gmail.com>
2014-02-12 18:56:06 -0800

    Update hypervisor_jan_2014.c

--- Hypervisor/hypervisor_jan_2014/hypervisor_jan_2014.c
+++ Hypervisor/hypervisor_jan_2014/hypervisor_jan_2014.c
@@ -1,16 +1,16 @@
 /*
- * hypervisor_jan_2014.c
- *
- * Created: 1/15/2014 3:28:22 PM
- * Author: David Muller
- * Revision : Muhsin Gurel 01/22/2014
- */ 
+* hypervisor_jan_2014.c
+*
+* Created: 1/15/2014 3:28:22 PM
+* Author: David Muller
+* Revision : Muhsin Gurel 01/29/2014
+*/
 
 
-#define OSCSPEED        16*1000*1000                /* in Hz */
+#define OSCSPEED 8*1000*1000 /* in Hz */
 
 #ifndef F_CPU
-#define F_CPU 16000000UL
+#define F_CPU 8000000UL
 #endif
 
 #include <string.h>
@@ -48,9 +48,11 @@ ISR(TIMER0_OVF_vect)
 }
 
 //Interrupt Service Routine for INT0...Trailmaster
-ISR(INT0_vect)
+ISR(INT2_vect)
 {
+	
 	set_state(ON);
+	
 }
 /********************************************************************************/
 
@@ -58,30 +60,31 @@ ISR(INT0_vect)
 
 void TIMER_Init(void)
 {
-	TCCR0 = (1<<CS02)|(1<<CS00);        // Timer Clock = system clock / 1024
-	TIFR  = 1<<TOV0;                    // Clear TOV0 / Clear pending interrupts
-	TIMSK = 1<< TOIE0;                  // Enable Timer 0 Overflow Interrupt
+	TCCR0 = (1<<CS02)|(1<<CS00); // Timer Clock = system clock / 1024
+	TIFR = 1<<TOV0; // Clear TOV0 / Clear pending interrupts
+	TIMSK = 1<< TOIE0; // Enable Timer 0 Overflow Interrupt
 }
 
 
 
 /*
- * Set up INT0 for Trailmaster interrupt.
- */
+* Set up INT0 for Trailmaster interrupt.
+*/
 void set_up_interrupt_pin()
 {
-	GICR |= 1<<INT0;		// Enable INT0
-	MCUCR &= ~(1<<ISC01) ;	// Trigger INT0 on falling edge
-	MCUCR &= ~(0<<ISC00) ;
-	sei();				//Enable Global Interrupt
+	
+	MCUCSR &= ~(1<<ISC2) ;        // Trigger INT2 on falling edge
+	GICR |= 1<<INT2;                // Enable INT2
+	//MCUCR &= ~(1<<ISC00) ;
+	sei();                                //Enable Global Interrupt
 }
 
 
 
 /*
- * "Power-down" sleep mode.
- */
-void power_down_sleep_mode() 
+* "Power-down" sleep mode.
+*/
+void power_down_sleep_mode()
 {
 	// IN THE FUTURE ALSO TURN OFF THE JTAG (set JTD bit in MCUCSR to 1)... for now, we'll leave JTAG on out of convenience
 	set_sleep_mode(SLEEP_MODE_PWR_DOWN);
@@ -93,7 +96,7 @@ void power_down_sleep_mode()
 
 void power_off_components()
 {
-	WDT_off();  //make sure the watchdog is off
+	WDT_off(); //make sure the watchdog is off
 }
 
 
@@ -102,6 +105,8 @@ void power_off_components()
 int main(void)
 {
 	int current_state;
+	unsigned char old_value;
+	unsigned char new_value;
 	
 	// initialize input/output gpios, UART, and the timer
 	PORT_Init();
@@ -111,32 +116,48 @@ int main(void)
 	// make sure all extra components (watchdog,...) are off
 	power_off_components();
 	
-	// set up INT0 for Trailmaster input
+	// set up INT2 for Trailmaster input
 	set_up_interrupt_pin();
-	
+				
 	while(1)
 	{
 		current_state = get_state();
-				
+		
 		if (current_state == ON)
 		{
+		
 			PrintLn("ON");
+			relayON1(ON); // Main System Power
 			turn_on_bbb();
-			turn_on_gopro();  // requires a 3 second GPIO hold, so we turn on GoPro after flipping the BBB relay
-			set_bbb_heart_beat(ON); 
+			relayON2(ON); // GoPro Power
+			turn_on_gopro(); // requires a half second GPIO hold, so we turn on GoPro after flipping the BBB relay
+			set_bbb_heart_beat(ON);
+			_delay_ms(5000);
+			old_value = readBBBHeartBeatLine(); 
 			while((!(check_if_bbb_is_on())) && get_bbb_heart_beat() )
 			{
-				// Wait for BBB shutdown signal or BBB heartbeat to stop ticking
+				new_value = readBBBHeartBeatLine(); 
+				if (old_value !=new_value) //say the line is normally high, but goes low to indicate a pulse
+
+				{
+					set_bbb_heart_beat(ON);
+
+				}
 			}
+
+
 			set_state(OFF);
 			turn_off_bbb();
-			turn_off_gopro();  // again, GoPro requires a 3 second GPIO hold, so do after the BBB
+			turn_off_gopro(); // again, GoPro requires a 2 second GPIO hold, so do after the BBB
+			relayON2(OFF);  // GoPro Power Off
+			relayON1(OFF); //Main System Power Off
 		}
 		
 		else if (current_state == OFF)
 		{
 			PrintLn("OFF!");
 			power_down_sleep_mode();
+			
 		}
 	}
 }
